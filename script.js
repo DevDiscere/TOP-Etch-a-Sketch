6b8201dc6cfd63cb19b31ba7963e4db4aa5f547d
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-02 20:06:19 +0800

    Modify HTML structure, CSS styles, and JS script
    I placed the reset and generate buttons inside a div to utilize flexbox.
    
    I removed and added some styles in CSS. Instead of using gap, I Instead
    used outline so that it is not hard on the eyes.
    
    I removed the calculateContainerWidth function and instead calculate the
    dimensions of the grid cell dynamically. I also followed Airbnb's
    JavaScript style guide to clean up some of my code.

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
 const generateGrid = function generateGrid (gridContainer, gridSize) {
-    const calculateContainerWidth = function calculateContainerWidth (cellWidth, gridGap, gridSize) {
-        return ((cellWidth + gridGap) * gridSize - gridGap) + "px"
-    }
-
     const giveCellsHoverEffect = function giveCellsHoverEffect () {
         const gridCells = document.querySelectorAll(".grid-cell");
 
         // Add hovering effect on each cell
         gridCells.forEach( (cell) => {
             cell.addEventListener("mouseenter", (event) => {
-                event.target.style.backgroundColor = "blue"
+                if (event.shiftKey) {  
+                    event.target.style.backgroundColor = "blue";
+                }
             });
         });
     }
 
-    // TASK: Make this dynamic
-    const WIDTH = 45;
-    const HEIGHT = WIDTH;
-    const GAP = 8;
+    const containerWidth = Number(window.getComputedStyle(gridContainer).width.slice(0, -2));
     const gridDimension = gridSize * gridSize;
+    const cellWidth = containerWidth / gridSize;
+    const cellHeight = cellWidth;
 
     // Clear grid
     while (gridContainer.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
 
-    // Calculate container width
-    gridContainer.style.width = calculateContainerWidth(WIDTH, GAP, gridSize);
-
     // Generate grid cells
     for (let i = 0; i < gridDimension; i++) {
         const gridCell = document.createElement("div");
 
         // Convert inputted width and height pixel values into string
-        gridCell.style.width = WIDTH + "px";
-        gridCell.style.height = HEIGHT + "px";
+        gridCell.style.width = cellWidth + "px";
+        gridCell.style.height = cellHeight + "px";
         gridCell.classList.add("grid-cell");
 
         gridContainer.appendChild(gridCell);
@@ -44,21 +38,21 @@ const generateGrid = function generateGrid (gridContainer, gridSize) {
 }
 
 const getNewGridSize = function getNewGridSize () {
+    const maximumGridSize = 100;
     let promptText = "Define a new grid size (16-100): ";
     let newGridSize = 0;
 
     while (true) {
-        const MAX_GRID_SIZE = 100;
-        let userInput = prompt(promptText);
+        const userInput = prompt(promptText);
 
         newGridSize = Number(userInput);
 
         if (!Number.isInteger(newGridSize)) {
             promptText = `${newGridSize} is not an integer.\n\nDefine a new grid size (16-100): `;
-        } else if (newGridSize < MINIMUM_GRID_SIZE) {
-            promptText = `${newGridSize} is less than the minimum grid size of ${MINIMUM_GRID_SIZE}.\n\nDefine a new grid size (16-100): `;
-        } else if (newGridSize > MAX_GRID_SIZE) {
-            promptText = `${newGridSize} is greater than the maximum grid size of ${MAX_GRID_SIZE}.\n\nDefine a new grid size (16-100): `
+        } else if (newGridSize < minimumGridSize) {
+            promptText = `${newGridSize} is less than the minimum grid size of ${minimumGridSize}.\n\nDefine a new grid size (16-100): `;
+        } else if (newGridSize > maximumGridSize) {
+            promptText = `${newGridSize} is greater than the maximum grid size of ${maximumGridSize}.\n\nDefine a new grid size (16-100): `
         } else {
             break;
         }   
@@ -67,14 +61,14 @@ const getNewGridSize = function getNewGridSize () {
     return newGridSize;
 }
 
-const MINIMUM_GRID_SIZE = 16;
+const minimumGridSize = 16;
 const mainContainer = document.querySelector(".main-container");
 const resetButton = document.querySelector(".reset-button");
 const generateButton = document.querySelector(".generate-button");
 
-generateGrid(mainContainer, MINIMUM_GRID_SIZE);
+generateGrid(mainContainer, minimumGridSize);
 
 generateButton.addEventListener("click", () => {
-    let newGridSize = getNewGridSize();
+    const newGridSize = getNewGridSize();
     generateGrid(mainContainer, newGridSize);
 });
