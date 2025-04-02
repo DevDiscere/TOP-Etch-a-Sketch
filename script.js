97f62946b55fa81026716cc0903f8894e531370b
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-02 14:06:00 +0800

    Add main HTML structure, CSS styles, and JS script
    I added the main HTML structure of the Etch-a-Sketch project by creating
    a header, a main container where the grid cells are going to be placed,
    and a footer.
    
    As for the CSS styles, I once again utilized Josh W. Comeau's CSS reset
    and added basic design styles and flexbox layout to the content. This
    will be polished further once the functionality of the script is
    complete.
    
    For the JS script, I initialized two function expressions: one for
    calculating container width, and one for generating the grid cells. I
    added parameters to these functions so I can flexibly utilize them
    instead of manually changing the values.

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const p = document.querySelector("p");
-p.style.fontSize = "3rem";
+const calculateContainerWidth = function calculateContainerWidth (gridContainer, gridWidth, gridGap) {
+    const GRID_ROW_LENGTH = 16;
+    // Convert computed width pixel value into string 
+    gridContainer.style.width = ((gridWidth + gridGap) * GRID_ROW_LENGTH - gridGap) + "px";
+}
+
+const generateGridCells = function generateGridCells (cellHeight, cellWidth, gridSize = 16) {
+    const DEFAULT_GRID_SIZE = gridSize * gridSize;
+
+    for (let i = 0; i < DEFAULT_GRID_SIZE; i++) {
+        let gridCell = document.createElement("div");
+        // Convert inputted width and height pixel values into string
+        gridCell.style.width = cellWidth + "px";
+        gridCell.style.height = cellHeight + "px";
+        gridCell.classList.add("grid-cell");
+        mainContainer.appendChild(gridCell);
+    }
+}
+
+const WIDTH = 45;
+const HEIGHT = WIDTH;
+const GAP = 8;
+const mainContainer = document.querySelector(".main-container");
+
+calculateContainerWidth(mainContainer, WIDTH, GAP);
+generateGridCells(HEIGHT, WIDTH);
