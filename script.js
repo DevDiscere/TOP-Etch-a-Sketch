a6be7c069c04e9d43f64c1f212dc9a8ad9f591cb
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-02 18:03:49 +0800

    Add two buttons in HTML and modify JS script
    I added two buttons in the HTML structure, one for resetting the grid
    back to its initial state, and one for defining new grid size.
    
    In the JS script, I refactored the function for generating the grid to
    contain other relevant functions such as calculating container width and
    giving cells a hover effect.
    
    I also created a function for the generate button to generate a new grid
    with the user's specified size. I added input validations to ensure that
    the specified size is only an integer, not less than the minimum grid
    size, and not greater than the maximum grid size. This is accompanied by
    a custom prompt text to let the user know why their input was not
    accepted.

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
-const calculateContainerWidth = function calculateContainerWidth (gridContainer, gridWidth, gridGap) {
-    const GRID_ROW_LENGTH = 16;
-    // Convert computed width pixel value into string 
-    gridContainer.style.width = ((gridWidth + gridGap) * GRID_ROW_LENGTH - gridGap) + "px";
-}
-
-const generateGridCells = function generateGridCells (cellHeight, cellWidth, gridSize = 16) {
-    const DEFAULT_GRID_SIZE = gridSize * gridSize;
-
-    for (let i = 0; i < DEFAULT_GRID_SIZE; i++) {
-        let gridCell = document.createElement("div");
-        // Convert inputted width and height pixel values into string
-        gridCell.style.width = cellWidth + "px";
-        gridCell.style.height = cellHeight + "px";
-        gridCell.classList.add("grid-cell");
-        mainContainer.appendChild(gridCell);
+const generateGrid = function generateGrid (gridContainer, gridSize) {
+    const calculateContainerWidth = function calculateContainerWidth (cellWidth, gridGap, gridSize) {
+        return ((cellWidth + gridGap) * gridSize - gridGap) + "px"
     }
+
+    const giveCellsHoverEffect = function giveCellsHoverEffect () {
+        const gridCells = document.querySelectorAll(".grid-cell");
+
+        // Add hovering effect on each cell
+        gridCells.forEach( (cell) => {
+            cell.addEventListener("mouseenter", (event) => {
+                event.target.style.backgroundColor = "blue"
+            });
+        });
+    }
+
+    // TASK: Make this dynamic
+    const WIDTH = 45;
+    const HEIGHT = WIDTH;
+    const GAP = 8;
+    const gridDimension = gridSize * gridSize;
+
+    // Clear grid
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    // Calculate container width
+    gridContainer.style.width = calculateContainerWidth(WIDTH, GAP, gridSize);
+
+    // Generate grid cells
+    for (let i = 0; i < gridDimension; i++) {
+        const gridCell = document.createElement("div");
+
+        // Convert inputted width and height pixel values into string
+        gridCell.style.width = WIDTH + "px";
+        gridCell.style.height = HEIGHT + "px";
+        gridCell.classList.add("grid-cell");
+
+        gridContainer.appendChild(gridCell);
+    }
+
+    giveCellsHoverEffect();
 }
 
-const changeCellColor = function changeCellColor (event) {
-    const currentCell = event.target;
+const getNewGridSize = function getNewGridSize () {
+    let promptText = "Define a new grid size (16-100): ";
+    let newGridSize = 0;
 
-    currentCell.style.backgroundColor = "blue";
+    while (true) {
+        const MAX_GRID_SIZE = 100;
+        let userInput = prompt(promptText);
+
+        newGridSize = Number(userInput);
+
+        if (!Number.isInteger(newGridSize)) {
+            promptText = `${newGridSize} is not an integer.\n\nDefine a new grid size (16-100): `;
+        } else if (newGridSize < MINIMUM_GRID_SIZE) {
+            promptText = `${newGridSize} is less than the minimum grid size of ${MINIMUM_GRID_SIZE}.\n\nDefine a new grid size (16-100): `;
+        } else if (newGridSize > MAX_GRID_SIZE) {
+            promptText = `${newGridSize} is greater than the maximum grid size of ${MAX_GRID_SIZE}.\n\nDefine a new grid size (16-100): `
+        } else {
+            break;
+        }   
+    }
+
+    return newGridSize;
 }
 
-const WIDTH = 45;
-const HEIGHT = WIDTH;
-const GAP = 8;
+const MINIMUM_GRID_SIZE = 16;
 const mainContainer = document.querySelector(".main-container");
+const resetButton = document.querySelector(".reset-button");
+const generateButton = document.querySelector(".generate-button");
 
-calculateContainerWidth(mainContainer, WIDTH, GAP);
-generateGridCells(HEIGHT, WIDTH);
+generateGrid(mainContainer, MINIMUM_GRID_SIZE);
 
-const gridCells = document.querySelectorAll(".grid-cell");
-
-// Add hovering effect on each cell
-gridCells.forEach( (cell) => {
-    cell.addEventListener("mouseenter", (event) => {
-        changeCellColor(event);
-    });
+generateButton.addEventListener("click", () => {
+    let newGridSize = getNewGridSize();
+    generateGrid(mainContainer, newGridSize);
 });
